2351eaf4f94f70250f806e5c8a1ab22d7297d4c4
prasanjitsarker3 <sarkerprasanjit379@gmail.com>
2023-01-23 21:08:40 +0530

    Code Done

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        color:{
+            yellow: '#FECC02',
+            white: '#FFFFFF',
+            gray: '#4E4E4E',
+            black: '#131313',
+        }
+      },
+       
+    },
+    plugins: [],
+  }
